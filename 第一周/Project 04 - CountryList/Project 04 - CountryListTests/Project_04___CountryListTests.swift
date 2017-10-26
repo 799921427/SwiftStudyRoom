c791adf474b69a9356f51c9913f2161a1307068c
System Administrator <root@Mac-mini.local>
2017-10-26 20:59:02 +0800

    #

---
+++ 第一周/Project 04 - CountryList/Project 04 - CountryListTests/Project_04___CountryListTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_04___CountryListTests.swift
+//  Project 04 - CountryListTests
+//
+//  Created by 张德茂 on 2017/10/21.
+//  Copyright © 2017年 张德茂. All rights reserved.
+//
+
+import XCTest
+@testable import Project_04___CountryList
+
+class Project_04___CountryListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
